2b132b36d3a564ce15ff7b385d0063b7195f6d6b
Alexey Kazimirov <a.kazimirov@gmail.com>
2019-11-13 10:27:41 +0800

    Update 2 knn.py

--- scikit-learn/2 knn.py
+++ scikit-learn/2 knn.py
@@ -12,12 +12,15 @@ Xtest = test.values[:,:-1]
 ytest = test.values[:,-1:]
 
 def accuracy(y1, y2):
-    ...
-    return ...
+    count = 0
+    for i in range(len(y1)):
+        if (y1[i] >= 0.5) and (y2[i] >= 0.5) or (y1[i] < 0.5) and (y2[i] < 0.5):
+            count += 1
+    return 100 * count / len(y1)
 
 n_neighbors = 15
 clf = neighbors.KNeighborsClassifier(n_neighbors, weights='uniform')
 clf.fit(Xtrain, ytrain.ravel())
 Z = clf.predict(Xtest)
-print(sum(abs(Z-ytest.ravel()))/len(Z))
-#print(accuracy(Z, ytest.ravel())
+#print(sum(abs(Z-ytest.ravel()))/len(Z))
+print(accuracy(Z, ytest.ravel())
